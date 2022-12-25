ef921a0a137c19d883ab09e5343a2c63f20851b7
Thorsten Schminkel <5998952+schminkel@users.noreply.github.com>
2022-12-25 17:59:47 +0100

    002 text change

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            001 Get started by editing&nbsp;
+            002 Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
